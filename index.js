4101a363f6bb3d1033779cca3bf084e3e112ddf9
simonihmig <ihmig@kaliber5.de>
2017-12-27 12:23:54 +0100

    First implementation

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 /* eslint-env node */
 'use strict';
 
+const VersionChecker = require('ember-cli-version-checker');
+const InElementTransform = require('./lib/in-element-transform');
+const InElementWormholeTransform = require('./lib/in-element-wormhole-transform');
+
+const MINIMUM_IN_ELEMENT_EMBER_VERSION = '2.10.0';
+
 module.exports = {
-  name: 'ember-in-element-polyfill'
+  name: 'ember-in-element-polyfill',
+
+  init() {
+    let checker = new VersionChecker(this);
+    this.emberDep = checker.forEmber();
+    this._super.init.apply(this, arguments);
+  },
+
+  setupPreprocessorRegistry(type, registry) {
+    registry.add('htmlbars-ast-plugin', {
+      name: 'ember-in-element-polyfill',
+      plugin: this.needsWormholePolyfill() ? InElementWormholeTransform : InElementTransform,
+      baseDir() {
+        return __dirname;
+      },
+      cacheKey() {
+        return 'ember-in-element-polyfill';
+      },
+    });
+  },
+
+  shouldIncludeChildAddon(addon) {
+    if (addon.name === 'ember-wormhole') {
+      return this.needsWormholePolyfill();
+    }
+    return this._super.shouldIncludeChildAddon.apply(this, arguments);
+  },
+
+  needsWormholePolyfill() {
+    return this.emberDep.lt(MINIMUM_IN_ELEMENT_EMBER_VERSION);
+  }
 };
---
+++ tests/integration/in-element-test.js
@@ -0,0 +1,21 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | in-element', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('renders into destination element', async function(assert) {
+    await render(hbs`
+      <div id="test-destination-element"></div>
+      {{#if ready}}
+        {{#in-element destinationElement}}Some text{{/in-element}}
+      {{/if}}
+    `);
+    this.set('destinationElement', this.element.querySelector('#test-destination-element'));
+    this.set('ready', true);
+    assert.dom('#test-destination-element').containsText('Some text', 'The content has been rendered in the destination element');
+  });
+
+});
