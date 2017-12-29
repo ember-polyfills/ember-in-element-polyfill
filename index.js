7095e2c1f7f197d1622f4a3639e8aa9dab764e2a
simonihmig <ihmig@kaliber5.de>
2017-12-29 22:52:44 +0100

    Prepare for Ember w/ public in-element

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const VersionChecker = require('ember-cli-version-checker');
 const InElementTransform = require('./lib/in-element-transform');
 const InElementWormholeTransform = require('./lib/in-element-wormhole-transform');
 
-const MINIMUM_IN_ELEMENT_EMBER_VERSION = '2.10.0';
+const MINIMUM_PRIVATE_IN_ELEMENT_EMBER_VERSION = '2.10.0';
+const MINIMUM_PUBLIC_IN_ELEMENT_EMBER_VERSION = '10.0.0'; // t.b.d
 
 module.exports = {
   name: 'ember-in-element-polyfill',
 
   setupPreprocessorRegistry(type, registry) {
+    if (this.hasPublicInElement()) {
+      return;
+    }
+
     registry.add('htmlbars-ast-plugin', {
       name: 'ember-in-element-polyfill',
-      plugin: this.needsWormholePolyfill() ? InElementWormholeTransform : InElementTransform,
+      plugin: this.hasPrivateInElement() ? InElementTransform : InElementWormholeTransform,
       baseDir() {
         return __dirname;
       },
@@ -25,16 +30,27 @@ module.exports = {
 
   shouldIncludeChildAddon(addon) {
     if (addon.name === 'ember-wormhole') {
-      return this.needsWormholePolyfill();
+      return !this.hasPrivateInElement() && !this.hasPublicInElement();
     }
     return this._super.shouldIncludeChildAddon.apply(this, arguments);
   },
 
-  needsWormholePolyfill() {
-    if (!this.emberDep) {
+  hasPublicInElement() {
+    // @todo once https://github.com/emberjs/rfcs/pull/287 is accepted and implemented, this function should return
+    // true for any Ember version that already ships a public `in-element`!
+    this.ensureEmberVersion();
+    return this.emberVersion.gte(MINIMUM_PUBLIC_IN_ELEMENT_EMBER_VERSION);
+  },
+
+  hasPrivateInElement() {
+    this.ensureEmberVersion();
+    return this.emberVersion.gte(MINIMUM_PRIVATE_IN_ELEMENT_EMBER_VERSION);
+  },
+  
+  ensureEmberVersion() {
+    if (!this.emberVersion) {
       let checker = new VersionChecker(this);
-      this.emberDep = checker.forEmber();
+      this.emberVersion = checker.forEmber();
     }
-    return this.emberDep.lt(MINIMUM_IN_ELEMENT_EMBER_VERSION);
   }
 };
