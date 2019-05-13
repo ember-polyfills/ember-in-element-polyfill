92de6fc3a96d4b32bb7dec82dc02ec9c6d06c343
simonihmig <simon.ihmig@kaliber5.de>
2019-05-13 20:22:53 +0200

    Run codemods

--- tests/integration/maybe-in-element-test.js
+++ tests/integration/maybe-in-element-test.js
@@ -1,18 +1,20 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('maybe-in-element', 'Integration | maybe-in-element', {
-  integration: true
-});
+module('Integration | maybe-in-element', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('works with maybe-in-element', function(assert) {
-  this.render(hbs`
-      <div id="test-destination-element"></div>
-      {{#if ready}}
-        {{#maybe-in-element destinationElement false}}Some text{{/maybe-in-element}}
-      {{/if}}
-    `);
-  this.set('destinationElement', document.querySelector('#test-destination-element'));
-  this.set('ready', true);
-  assert.dom('#test-destination-element').containsText('Some text', 'The content has been rendered in the destination element');
+  test('works with maybe-in-element', async function(assert) {
+    await render(hbs`
+        <div id="test-destination-element"></div>
+        {{#if ready}}
+          {{#maybe-in-element destinationElement false}}Some text{{/maybe-in-element}}
+        {{/if}}
+      `);
+    this.set('destinationElement', document.querySelector('#test-destination-element'));
+    this.set('ready', true);
+    assert.dom('#test-destination-element').containsText('Some text', 'The content has been rendered in the destination element');
+  });
 });
